6e9ffbb416dbe48fa1d2e173e5e278bf8b10024c
djorgeac <djorgeac@edu.uwaterloo.ca>
2019-02-21 13:35:29 -0500

    exponenbackoff

--- src/lab02/node.java
+++ src/lab02/node.java
@@ -8,6 +8,9 @@ import java.util.List;
 public class node {
     public PriorityQueue<arrivalEvent> eventQueue;
     private int collisionCrt;
+    double timeDelay = 0.0;
+    private static double TIME_P = 5.12e-4;
+    int r = 0;
 
     public node(double arrivalRate, int rt, int packetSize) {
         List<arrivalEvent> list 
@@ -16,7 +19,16 @@ public class node {
         collisionCrt = 0;
     }
 
-    public expoBackOff() {
+    public void expoBackOff() {
+    	collisionCrt++;
+    	if (collisionCrt>=10) {
+    		//collisionCrt = 0; I dunno if i have to do 
+    	} else {
+    		r=  (int) (Math.random()* (Math.pow(2,collisionCrt)-1));
+    		timeDelay = (double) r * TIME_P;
+    	}
+    	
+    	
     }
 
     
